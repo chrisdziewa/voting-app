a0f57a3047fef150af6b79a8618a7910276f6c31
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-23 15:40:54 -0600

    Add user id to post poll routes and only allow user to delete their own polls

--- app/routes/poll.routes.js
+++ app/routes/poll.routes.js
@@ -1,6 +1,8 @@
 'use strict';
 const Poll = require('../models/poll');
+const User = require('../models/user');
 const _ = require('underscore');
+const authenticateRoute = require('../middleware/auth-middleware');
 
 module.exports = function(express, app) {
   let router = express.Router();
@@ -10,24 +12,22 @@ module.exports = function(express, app) {
     Poll.find({}, (err, polls) => {
       if (err) {
         return res.status(500).send('There was an error processing your request');
-      }
-      else {
+      } else {
         res.json(polls);
       }
     });
   });
 
   // Create Post
-  router.post('/', (req, res) => {
+  router.post('/', authenticateRoute, (req, res) => {
     let validAttributes = {};
-    
+    let currentUser = req.decoded.username;
+
     if (typeof req.body.choices !== 'object' || req.body.choices.length < 2) {
       return res.status(400).send('A poll must have at least 2 options');
-    }
-    else if (typeof req.body.choices !== 'object' || req.body.choices.length > 40) {
+    } else if (typeof req.body.choices !== 'object' || req.body.choices.length > 40) {
       return res.status(400).send('A poll can have a maximum of 40 options');
-    }
-    else if (typeof req.body.question !== 'string') {
+    } else if (typeof req.body.question !== 'string') {
       return res.status(400).send('Invalid question');
     }
 
@@ -41,13 +41,25 @@ module.exports = function(express, app) {
 
     validAttributes.choices = choices;
 
-    let poll = new Poll(validAttributes);
-
-    poll.save((err) => {
+    User.findOne({
+      username: currentUser
+    }, '_id', (err, user) => {
       if (err) {
-        return res.status(500).send(err);
+        return res.status(500).send('An error prevented the server from completing your request');
+      } else if (!user) {
+        return res.status(500).send('Could not complete your request');
       }
-      res.json(poll);
+      // Everything is good, create poll and put reference to user
+      validAttributes.user_id = user._id;
+      
+      let poll = new Poll(validAttributes);
+
+      poll.save((err) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        res.json(poll);
+      });
     });
   });
 
@@ -55,15 +67,19 @@ module.exports = function(express, app) {
   router.get('/:id', (req, res) => {
     let pollId = req.params.id;
 
-    Poll.findById({_id: pollId}, (err, poll) => {
+    Poll.findById({
+      _id: pollId
+    }, (err, poll) => {
       if (err) {
         return res.status(500).send('There was an error completing your request');
       }
       if (!poll) {
         // No poll found 
-        res.status(404).json({message: 'No poll with that id found'});
+        res.status(404).json({
+          message: 'No poll with that id found'
+        });
       }
-        res.json(poll);
+      res.json(poll);
     });
   });
 
@@ -72,51 +88,65 @@ module.exports = function(express, app) {
     let pollId = req.params.id;
     if (!typeof req.body.choice === 'string' || req.body.choice.trim().length === 0) {
       return res.status(400).send('Invalid data in request');
-    } 
+    }
     Poll.findById(pollId, (err, poll) => {
       if (err) {
         return res.status(500).send('There was an error completing your request');
-      } 
+      }
       if (!poll) {
         return res.status(404).send('No poll exists with that id');
-      } 
+      }
       // Poll found now update it
       let choice = req.body.choice.trim();
 
       let newItem = true;
       Object.keys(poll.choices).forEach((propertyName) => {
         if (propertyName.toLowerCase() === choice.toLowerCase() && newItem) {
-          poll.choices[propertyName] = poll.choices[propertyName] + 1;  
+          poll.choices[propertyName] = poll.choices[propertyName] + 1;
           newItem = false;
         }
       });
       if (newItem) {
         poll.choices[choice] = 1;
       }
-      Poll.update({_id: pollId}, poll, (err, obj) => {
+      Poll.update({
+        _id: pollId
+      }, poll, (err, obj) => {
         if (err || obj.n !== 1) {
           return res.status(500).send('Could not cast your vote');
-        } 
+        }
         res.json(poll);
       });
     });
   });
 
   // Delete a poll
-  router.delete('/:id', (req, res) => {
+  router.delete('/:id', authenticateRoute, (req, res) => {
+    let currentUser = req.decoded.username;
     let pollId = req.params.id;
 
-    Poll.remove({_id: pollId}, (err, obj) => {
+    User.findOne({
+      username: currentUser
+    }, '_id' ,(err, user) => {
       if (err) {
-        console.log(err);
-        return res.status(500).send('Could not delete poll');
+        return res.status(500).send('An error prevented your request from being made');
+      } else if (!user) {
+        return res.status(400).send('Bad data request');
       }
-      if (obj.result.n === 0) {
-        return res.status(404).send('Poll with that id was not found');
-      }
-      else {
-        res.send('Poll has been deleted');
-      }
+      // User was successfully found, now try to delete item
+      Poll.remove({
+        _id: pollId,
+        user_id: user._id
+      }, (err, obj) => {
+        if (err) {
+          return res.status(500).send('Could not delete poll');
+        }
+        if (obj.result.n === 0) {
+          return res.status(404).send('Poll with that id was not found');
+        } else {
+          res.send('Poll has been deleted');
+        }
+      });
     });
   });
 
