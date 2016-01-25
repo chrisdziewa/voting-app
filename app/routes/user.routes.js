30a2a5d581a6046cfc7490d54efceabbc3393d49
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-24 18:06:15 -0600

    Add authentication to user routes

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,6 +3,9 @@ const User = require('../models/user');
 const Poll = require('../models/poll');
 const _ = require('underscore');
 const authenticateRoute = require('../middleware/auth-middleware');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
 
 // All routes are starting at /api/users/
 
@@ -10,11 +13,11 @@ module.exports = function(express, app) {
   let router = express.Router();
 
   // => GET /api/users
-  router.get('/', (req,res) => {
+  router.get('/', (req, res) => {
     User.find({}, (error, users) => {
       if (error) {
         return res.status(500).send('There was an error processing your request');
-      } 
+      }
       if (users) {
         res.send(users);
       } else {
@@ -24,7 +27,7 @@ module.exports = function(express, app) {
   });
 
   // Create a new user => POST /api/users
-  router.post('/', (req,res) => {
+  router.post('/', (req, res) => {
     let validAttributes = _.pick(req.body, 'username', 'email', 'password');
     let newUser = new User(validAttributes);
 
@@ -33,14 +36,23 @@ module.exports = function(express, app) {
         // Duplicate database value
         if (error.code == 11000) {
           if (/username/.test(error.message) || /lowercase_name/.test(error.message)) {
-            return res.status(500).json({success: false, message: 'User with that name already exists'});
-          }
-           else { 
-            return res.status(500).json({success: false, message: 'Email is already taken',  error: error.message});
+            return res.status(500).json({
+              success: false,
+              message: 'User with that name already exists'
+            });
+          } else {
+            return res.status(500).json({
+              success: false,
+              message: 'Email is already taken',
+              error: error.message
+            });
           }
         } else {
-           return res.status(500).json({success: false, message: error});
-         }
+          return res.status(500).json({
+            success: false,
+            message: error
+          });
+        }
       } else {
         // Prevent password field from being returned in response
         let displayUser = _.pick(user, '_id', 'username', 'email');
@@ -50,28 +62,35 @@ module.exports = function(express, app) {
   });
 
   // => GET /api/users/:id
-  router.get('/:id', authenticateRoute,(req, res) => {
+  router.get('/:id', authenticateRoute, (req, res) => {
     let userId = req.params.id
-    User.findById({_id: userId}, (err, user) => {
+    User.findById({
+      _id: userId
+    }, (err, user) => {
       if (err) {
         return res.status(500).send('Error completing your request');
-      } 
-      else if (!user) {
+      } else if (!user) {
         return res.status(404).send("User not found");
       }
-        res.json(user);
+      res.json(user);
     });
   });
 
   // => PUT /api/users/:id
-  router.put('/:id', authenticateRoute,(req, res) => {
+  router.put('/:id', authenticateRoute, (req, res) => {
     let userId = req.params.id;
     let validAttributes = _.pick(req.body, 'username', 'email', 'password');
-    User.findById({_id: userId}, (err, user) => {
+    User.findById({
+      _id: userId
+    }, (err, user) => {
       if (err) {
         return res.status(500).send('There was an error completing your request');
       }
       if (user) {
+        // If not the current logged in user
+        if (user.username !== req.decoded.username) {
+          return res.status(401).send('You may not update other profiles');
+        }
         // If user found, set only the new attributes
         if (validAttributes.username) user.username = validAttributes.username;
         if (validAttributes.email) user.email = validAttributes.email;
@@ -85,29 +104,84 @@ module.exports = function(express, app) {
         if (err) {
           res.status(500).send('Could not update user');
         } else {
-          res.json({success: true, message: 'User has been updated!'});
+          // create new token
+          let token = jwt.sign({
+            username: user.username,
+            email: user.email
+          }, config.privateKey, {
+            expiresIn: '7d'
+          });
+          // Update cookie to be correct username
+          res.cookie('auth_token', false, {
+            maxAge: 1,
+            path: "/"
+          });
+          res.clearCookie('auth_token', {
+            path: "/"
+          });
+          res.cookie('auth_token', token, {
+              maxAge: 604800000,
+              path: "/"
+            })
+            .json({
+              success: true,
+              message: 'User has been updated!'
+            });
         }
       });
     });
   });
 
   // => DELETE /api/users/:id
-  router.delete('/:id', authenticateRoute,(req, res) => {
+  router.delete('/:id', authenticateRoute, (req, res) => {
     let userId = req.params.id;
 
-    User.remove({_id: userId}, (err, obj) => {
+    User.findOne({username: req.decoded.username}, (err, user) => {
       if (err) {
-        return res.status(500).send('Could not delete user');
+        return res.status(500).send('There was an error completing your request');
+      } else if (!user) {
+        return res.status(404).send('No user with that id');
       }
-      if (obj.result.n === 0) {
-        return res.status(404).send('User with that id was not found');
+      // user found, check if it is the logged in user
+      if (userId !=  user._id) {
+        return res.status(401).send('You can only delete your own account');
       }
-      else {
-        res.send('User has been deleted');
-      }
+
+      // User is the valid user, now delete user 
+      User.remove({
+        _id: userId
+      }, (err, obj) => {
+        if (err || obj.n < 1) {
+          return res.status(500).send('Error removing account');
+        }
+        //  user removed,  now remove associated polls
+        Poll.remove({
+          user_id: userId
+        }, (err, obj) => {
+          if (err || obj.n >= 1) {
+            return res.status(500).json({
+              success: false,
+              message: 'Could not delete polls'
+            });
+          }
+           res.cookie('auth_token', false, {
+            maxAge: 1,
+            path: "/"
+          });
+          res.clearCookie('auth_token', {
+            path: "/"
+          });
+          res.json({
+            success: true,
+            message: 'Account has been deleted'
+          });
+        });
+      });
     });
   });
 
+
+  //==== Joint routes ====//
 
   // => GET /api/users/:id/polls
   router.get('/:username/polls', (req, res) => {
@@ -120,17 +194,16 @@ module.exports = function(express, app) {
     }, (err, user) => {
       if (err) {
         return res.status(500).send('Could not process your request');
-      }
-      else if (!user) {
+      } else if (!user) {
         return res.status(404).send('User does not exist');
-      } 
+      }
       // User found now find their polls
       Poll.find({
         user_id: user._id
       }, (err, polls) => {
         if (err) {
           res.status(500).send('Could not process your request');
-        } 
+        }
         // send back polls to user
         res.json(polls);
       });
@@ -138,16 +211,17 @@ module.exports = function(express, app) {
   });
 
   // => GET /api/users/:username/polls/:question
-  router.get('/:username/polls/:question', (req,res) => {
+  router.get('/:username/polls/:question', (req, res) => {
     let username = req.params.username.toLowerCase();
     let question = req.params.question;
-    User.findOne({lowercase_name: username}, (err, user) => {
+    User.findOne({
+      lowercase_name: username
+    }, (err, user) => {
       if (err) {
         return res.status(500).send('Could not process your request');
-      } 
-      else if (!user) {
+      } else if (!user) {
         return res.status(404).send('User does not exist');
-      } 
+      }
       // user found, now look for the poll
       let likeQuestion = new RegExp(question, 'i');
 
@@ -157,9 +231,8 @@ module.exports = function(express, app) {
       }, (err, poll) => {
         if (err) {
           return res.status(500).send('Could not process your request');
-        } 
-        else if (!poll) {
-         return res.status(404).send('Could not find poll with that name');
+        } else if (!poll) {
+          return res.status(404).send('Could not find poll with that name');
         }
         // User's poll found, not send it in response
         res.json(poll);
@@ -167,7 +240,5 @@ module.exports = function(express, app) {
     });
   });
 
-   return router;
+  return router;
 };
-
-
