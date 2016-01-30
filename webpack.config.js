c8d658126b79711c9ac1d589a41b47f0969e576b
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-30 13:10:58 -0600

    Add babel and sass processing with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   entry: [
     './public/src/index.js'
   ],
   output: {
-    path: __dirname + '/public/',
-    publicPath: '/public/',
-    filename: 'bundle.js'
+    filename: "./public/bundle.js"
   },
   module: {
-    loaders: [{
-      exclude: /(node_modules|app)/,
-      loader: 'babel'
-    }]
-  },
+  loaders: [
+    { 
+      test: /\.jsx?$/, 
+      exclude: ['node_modules', 'app'], 
+      loader: "babel-loader"
+    },
+    {
+        test: /\.scss$/,
+        exclude: ['node_modules', 'app'],
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+      }
+  ]
+},
+  plugins: [
+    new ExtractTextPlugin("./public/styles/main.css")
+  ],
   resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+    extensions: ['', '.js', '.jsx', '.scss']
   }
 };
