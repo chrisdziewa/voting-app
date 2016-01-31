a4bba5c0dc4f2b35f3f16aa1414e49b13cd93575
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-31 13:49:37 -0600

    Fix styling and class names on sign up form

--- public/src/components/signup-form.js
+++ public/src/components/signup-form.js
@@ -7,24 +7,30 @@ export default class SignupForm extends Component {
         <div className="form-container">
           <h3>Sign Up</h3>
           <form>
-            <label htmlFor="name" className="pull-left">Name</label>
-            <br/>
-            <input type="text" id="name" className="form-controls"/>
-            <br/>
-            <label htmlFor="email" className="pull-left">Email</label>
-            <br/>
-            <input type="text" id="email" className="form-controls"/>
-            <br/>
-            <label htmlFor="password" className="pull-left">Password</label>
-            <br/>
-            <input type="password" id="password" className="form-controls"/>
-            <br/>
-            <label htmlFor="confirm" className="pull-left">Confirm Password</label>
-            <br/>
-            <input type="password" id="confirm" className="form-controls"/>
-            <br/>
-            <input type="submit" className="pull-left form-controls btn btn-primary" value="Submit" />
-            <p className="pull-left">Already have an account?&nbsp;<a href="#">Login</a></p>
+            <div className="input-form-group">
+              <label htmlFor="name" className="pull-left">Name</label>
+              <br/>
+              <input type="text" id="name" className="form-control"/>
+            </div>
+            <div className="input-form-group">
+              <label htmlFor="email" className="pull-left">Email</label>
+              <br/>
+              <input type="text" id="email" className="form-control"/>
+            </div>
+            <div className="input-form-group">
+              <label htmlFor="password" className="pull-left">Password</label>
+              <br/>
+              <input type="password" id="password" className="form-control"/>
+            </div>
+            <div className="input-form-group">
+              <label htmlFor="confirm" className="pull-left">Confirm Password</label>
+              <br/>
+              <input type="password" id="confirm" className="form-control"/>
+            </div>
+            <div className="input-form-group">
+               <input type="submit" className="pull-left form-control btn btn-primary" value="Submit" />
+            </div>
+            <p className="pull-left">Already have an account?<a href="#"> Login</a></p>
           </form>
         </div>
       </div>
