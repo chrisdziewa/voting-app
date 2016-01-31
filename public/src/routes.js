f512e5dedfce3f193f65cba9494011734afa6cef
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-31 13:54:38 -0600

    Add login route to routes.js

--- public/src/routes.js
+++ public/src/routes.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Router, Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import Navbar from './components/navbar';
 import WelcomeBanner from './components/welcome-banner';
 import SignupForm from './components/signup-form';
+import LoginForm from './components/login-form';
 
 export default (
     <Route path="/" component={App}>
       <IndexRoute component={WelcomeBanner} />
       <Route path="/signup" component={SignupForm} />
+      <Route path="/login" component={LoginForm} />
     </Route>
 );
