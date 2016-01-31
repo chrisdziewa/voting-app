2ab7683f965639e76b3fced6f14223646d226aa6
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-31 13:37:31 -0600

    Add footer to all pages

---
+++ public/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default (props) => {
+  return (
+    <footer>
+      <div className="container">
+        <p className="text-center">&copy; 2016 Chris Dziewa</p>
+      </div>
+    </footer>
+  )
+}
