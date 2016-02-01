cf3e73ca4b174b30890d9c0df5e837eab3936e10
Chris Dziewa <chris.dziewa@gmail.com>
2016-01-31 18:23:09 -0600

    Add 404 page for routes that don't exist

---
+++ public/src/components/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="container-fluid page-not-found">
+        <h1>Uh oh...It looks like the page you requested doesn't exist!</h1>
+    </div>
+  );
+}
--- public/src/routes.js
+++ public/src/routes.js
@@ -6,11 +6,13 @@ import Navbar from './components/navbar';
 import HomePage from './components/home-page';
 import SignupForm from './components/signup-form';
 import LoginForm from './components/login-form';
+import NoMatch from './components/404';
 
 export default (
     <Route path="/" component={App}>
       <IndexRoute component={HomePage} />
       <Route path="/signup" component={SignupForm} />
       <Route path="/login" component={LoginForm} />
+      <Route path="*" component={NoMatch}/>
     </Route>
 );
