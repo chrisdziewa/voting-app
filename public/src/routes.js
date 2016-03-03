59c5098da7a59cbe0ec7bfdf5764fa318f609bf2
Chris Dziewa <chris.dziewa@gmail.com>
2016-03-03 13:15:36 -0600

    Change onEnter auth to use https instead of http

--- public/src/routes.js
+++ public/src/routes.js
@@ -31,7 +31,7 @@ export default function createRoutes(store) {
   }
 
   function checkAuth() {
-    axios.get('http://sondage-me.herokuapp.com/api/users/current')
+    axios.get('https://sondage-me.herokuapp.com/api/users/current')
       .then((response) => {
 
       }, () => {
