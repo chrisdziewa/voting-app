00df7890a3ce162f8430e58bde4c5bb802abf933
Chris Dziewa <chris.dziewa@gmail.com>
2016-03-03 12:49:10 -0600

    Add cors heder to all requests

--- server.js
+++ server.js
@@ -18,8 +18,13 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 // Allow access from different Origins
 
-app.options('*', cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+app.options('*', cors());
 
 app.use(express.static('public'));
 
