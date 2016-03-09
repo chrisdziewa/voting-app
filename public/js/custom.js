971658a8d4022784e5a43b00cc59ec30058d176e
Chris Dziewa <chris.dziewa@gmail.com>
2016-03-08 22:27:34 -0600

    Fix click on new poll button in mobile

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,7 +3,7 @@
 // code source: 
 //http://stackoverflow.com/questions/16877429/twitter-bootstrap-mobile-nav-hide-menu-after-clicking-menu-link
 $(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a') ) {
+    if( $(e.target).is('a') || $(e.target).is('span, i') ) {
         $(this).collapse('hide');
     }
 });
